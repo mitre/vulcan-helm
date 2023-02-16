52b378818564e1e98c560bb11b10cdf83d9c46fd
Vanessa Fotso <vfotso@mitre.org>
2023-02-16 17:12:41 -0500

    Update startup script to upgrade chart if it has already been installed
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- start_vulcan.sh
+++ start_vulcan.sh
@@ -3,5 +3,16 @@
 # Generate vulcan secrets
 ./generate-vulcan-secrets.sh
 
-# Install helm chart
-helm install vulcan .
+# Check if vulcan has already been released:
+if [ "$(helm ls -n vulcan -q)" = vulcan ]
+  then
+    echo "vulcan has already been installed."
+    echo ""
+    echo "Upgrading the release to a new version ..."
+    echo ""
+    # Upgrade chart if already installed
+    helm -n vulcan upgrade vulcan .
+  else
+    # Install helm chart
+    helm install -n vulcan --create-namespace vulcan .
+fi
